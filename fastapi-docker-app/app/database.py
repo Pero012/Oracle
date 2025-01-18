6356880931567d8d5c0056f3b19c8bdddae25bf7
Your Name <petar.eres@fpmoz.sum.ba>
2025-01-18 21:52:39 +0100

    Addec

--- fastapi-docker-app/app/database.py
+++ fastapi-docker-app/app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/project_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@mysql/project_db"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
